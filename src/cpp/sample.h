cec9f24fe8f81cb79d93561584c17524f8f6f17a
Tundra Fizz <MageLeif@Yahoo.com>
2019-02-07 18:40:10 -0800

    Clean up sample.h

--- src/cpp/sample.h
+++ src/cpp/sample.h
@@ -1,17 +1,6 @@
 #include "helper.h"
 #include <iostream>
 
-// #include <windows.h>
-// #include <windowsx.h>
-// #include <ole2.h>
-// #include <olectl.h>
-// #include <cmath>
-// #include <time.h>
-// #include <locale>
-// #include <gdiplus.h>
-
-// #pragma comment (lib, "Gdiplus.lib")
-
 struct ScreenCapture {
   static NAN_MODULE_INIT(Init);
   static NAN_METHOD(TakeScreenshot);
@@ -22,11 +11,7 @@ struct ScreenCapture {
 class MyAsyncWorker: public Nan::AsyncWorker{
   public:
 
-  // Constructor
   MyAsyncWorker(Nan::Callback *callback);
-  // MyAsyncWorker(std::string directory, Nan::Callback *callback);
-
-  // Mandatory special functions
   void Execute();          // Automatically called right after the Initializer
   void HandleOKCallback(); // Called once the program is completed
 };
